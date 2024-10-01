c49721cbe1978a9c4ad7bb5725f8b29c869ba39e
JBurke24 <160625000+JBurke24@users.noreply.github.com>
2024-10-01 12:04:39 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("My Streamlit App")
+
+col1 col2 = st.columns(2)
+
+with col1:
+    st.header("Column 1")
+    st.write("Hello")
+
+with col2: 
+    st.header("Column 2")
+    st.write("World")
+
+with st.expander("Expand for more information"):
+    st.write("Here you could put in some really, really explanatory stuff.")
